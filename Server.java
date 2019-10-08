1e6e7035422061d74af2147babd48d89de7fcb2d
29063116 <sipho.selai1999@gmail.com>
2019-10-08 12:21:33 +0200

    Initialized server

--- Server.java
+++ Server.java
@@ -9,12 +9,47 @@ public class Server
     private ServerSocket server = null;
     private DataInputStream in = null;
 
+    public Server(int port)
+    {
+       try {
 
+        server = new ServerSocket(port);
+        System.out.println("Server started");
+        System.out.println("Waiting for a client to connect....");
+
+        mySocket = server.accept();]
+        System.out.println("Client connection accepted");
+
+        in = new DataInputStream(new BufferedinputStream(mySocket.getInputStream()));
+        String line = "";
+
+        while(!line.equals("Done"))
+        {
+            try{
+                line = in.readUTF();
+                System.out.println(line);
+            }
+            catch(IOException i)
+            {
+              System.out.println(i);
+
+            }
+        }
+      
+        mySocket.close();
+        in.close();
+    }
+          
+        catch (Exception e)
+         {
+         System.out.println(i);
+       }
+    }
 }
 
     public static void main(String args[])
     {
         Server server = new Server(5000);
     }
-}
+
 
